398ef12a640e53990bcfcc1250f414c0c6202694
luislrs <luislirs@outlook.com>
2021-07-25 18:12:09 -0300

    Cadastro de produtos

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import Globalstyle from './globalStyle';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+      <Globalstyle/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
